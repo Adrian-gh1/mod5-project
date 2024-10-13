b2e2970c85ec0e5810bb3bd6dee7a8b04de4c58c
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-13 00:42:04 -0700

    Phase 3 - Complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -56,8 +56,15 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
-const initialState = { user: null };
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE'
+  });
+  dispatch(removeUser());
+  return response;
+};
 
+const initialState = { user: null };
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER:
