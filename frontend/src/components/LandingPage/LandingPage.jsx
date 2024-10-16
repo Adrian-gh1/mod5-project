840523e04bc00cdc9bb73f25a2c7ebb58def7cf1
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-15 21:36:20 -0700

    ID: 07 - Landing Page - List of All Spots

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
+import { FaStar } from 'react-icons/fa';
 import { allspots } from "../../store/spots";
 import './LandingPage.css';
 
@@ -22,7 +23,7 @@ const LandingPage = () => {
                         <img src={spot.previewImage} alt={spot.name} />
                         <div className='tile-info'>
                             <p>{spot.city}, {spot.state}</p>
-                            <p>{spot.aveReviews > 0 ? spot.aveRating.toFixed(2) : 'New'}</p>                                
+                            <p> <FaStar /> {spot.aveReviews > 0 ? spot.aveRating.toFixed(2) : 'New'}</p>                                
                         </div>
                         <p>${spot.price}/night</p>
                     </NavLink>
