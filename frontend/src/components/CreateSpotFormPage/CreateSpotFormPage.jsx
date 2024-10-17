9131f34e10d1cac936753a7f4b5d6cd09611001b
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-17 00:41:51 -0700

    ID: 27 - Create a New Spot

--- frontend/src/components/CreateSpotFormPage/CreateSpotFormPage.jsx
+++ frontend/src/components/CreateSpotFormPage/CreateSpotFormPage.jsx
@@ -1,15 +1,52 @@
 // frontend/src/components/CreateSpotFormPage/CreateSpotFormPage.jsx
 
-import { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import './CreateSpotFormPage.css';
 
 const CreateSpotFormPage = () => {
-
     const dispatch = useDispatch();
+    const [country, setCountry] = useState('');
+
 
     return (
         <div className='container'>
-            Create a New Spot
+            <h1>Create a New Spot</h1>
+            <h2>Where's your place located?</h2>
+            <p>Guests will only get your exact address once they booked a reservation.</p>
+            <form className="form-items">
+                <label>
+                    Country
+                    <input
+                        type="text"
+                        placeholder="Country"                        
+                    />
+                </label>
+
+                <label>
+                    Street Address
+                    <input
+                        type="text"
+                        placeholder="Street Address"                        
+                    />
+                </label>
+
+                <label>
+                    City
+                    <input
+                        type="text"
+                        placeholder="City"                        
+                    />
+                </label>
+
+                <label>
+                    State
+                    <input
+                        type="text"
+                        placeholder="State"                        
+                    />
+                </label>
+            </form>
         </div>
     );
 };
