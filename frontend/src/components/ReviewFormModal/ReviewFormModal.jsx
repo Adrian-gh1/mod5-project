0d28c6ee05da9db8c3f22a4e4a0d3f3d650fd7ed
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-18 19:47:16 -0700

    ID: 43 - Post a Review

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -3,16 +3,30 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
+import './ReviewFormModal.css'
 
 const ReviewFormModal = () => {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
+    const [review, setReview] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        closeModal();
+    };
     
     return (
-        <div>
-            Review Modal
-            <form>
-
+        <div className='review-form-container'>
+            <h3>How was your stay?</h3>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                    // rows="5"
+                    // style={{ width: '100%', padding: '10px' }}
+                    placeholder="Leave your review here..."                        
+                />
             </form>
         </div>
     );
