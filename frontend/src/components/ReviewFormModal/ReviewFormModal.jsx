8c1f4ea37cc3ec39b574b00e10d8bc3052da3833
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-18 19:59:43 -0700

    ID: 44 - Post a Review

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -3,12 +3,14 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
+import { FaRegStar } from 'react-icons/fa';
 import './ReviewFormModal.css'
 
 const ReviewFormModal = () => {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
     const [review, setReview] = useState('');
+    const [rating, setRating] = useState(1);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,14 +21,19 @@ const ReviewFormModal = () => {
         <div className='review-form-container'>
             <h3>How was your stay?</h3>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={review}
-                    onChange={(e) => setReview(e.target.value)}
-                    // rows="5"
-                    // style={{ width: '100%', padding: '10px' }}
-                    placeholder="Leave your review here..."                        
-                />
+                <div >
+                    <input
+                        type="text"
+                        value={review}
+                        onChange={(e) => setReview(e.target.value)}
+                        // rows="5"
+                        // style={{ width: '100%', padding: '10px' }}
+                        placeholder="Leave your review here..."                        
+                    />
+                </div>
+                <div className='star-rating'>
+                    <FaRegStar/> <FaRegStar/> <FaRegStar/> <FaRegStar/> <FaRegStar/> Stars
+                </div>
             </form>
         </div>
     );
