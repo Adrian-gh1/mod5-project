e7a6b695aa2b0edecc260ef4ed0b2ce752f58e96
Adrian-gh1 <Adrian22046@gmail.com>
2024-10-19 19:38:14 -0700

    ID: 57 - Manage Spots

--- frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
+++ frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
@@ -1,9 +1,11 @@
 // frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
 
 import { useEffect } from 'react';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { FaStar } from 'react-icons/fa';
 import { userSpots } from '../../store/spots';
+// import './ManageSpotsPage.css';
 
 const ManageSpotsPage = () => {
     const dispatch = useDispatch();
@@ -17,23 +19,36 @@ const ManageSpotsPage = () => {
     }, [dispatch, currentUser]);
 
     return (
-        <div>
+        <div className='tile-container'>
             <h2>Manage Spots</h2>
             {userSpotList.length > 0 ? (
-                userSpotList.map(spot => (
-                    <div key={spot.id}>
-                        {spot.name}
-                        {spot.city}
-                        {spot.state}
-                        {spot.price}
+                    <div className='tile-list'>
+                        {userSpotList.map(spot => (
+                            <div key={spot.id}>
+                                <div className='tile'>
+                                    <NavLink to={`/spots/${spot.id}`} title={spot.name}>
+                                        <img src={spot.previewImage} alt={spot.name} />
+                                        <div className='tile-info'>
+                                            <p>{spot.city}, {spot.state}</p>
+                                            <p> <FaStar /> {spot.avgRating > 0 ? spot.avgRating.toFixed(2) : 'New'}</p>                                
+                                        </div>
+                                        <p>${spot.price}/night</p>
+                                    </NavLink>
+                                </div>
+                                <div className='update-delete-buttons'>
+                                    <button>Update</button>
+                                    <button>Delete</button>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))
-
             ) : (
-                <div></div>
-            )}
+                <NavLink to="/spots/new" >
+                    Create a New Spot
+                </NavLink>
+            )}        
         </div>
-    );
+);
 };
 
 export default ManageSpotsPage;
